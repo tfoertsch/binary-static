0bc4520946fef5ffd6d123bb0f75cf6134931303
Torsten Förtsch <torsten@binary.com>
2015-10-11 14:05:19 +0000

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -12788,6 +12788,7 @@ WSTickDisplay.updateChart = function(data){
     "use strict";
 
     var reality_check_url = page.url.url_for('user/reality_check');
+    var reality_freq_url  = page.url.url_for('user/reality_check_frequency');
 
     function RealityCheck(cookieName, persistentStore, logoutLocation) {
         var val;
@@ -12798,6 +12799,11 @@ WSTickDisplay.updateChart = function(data){
         val = ($.cookie(this.cookieName)||'').split(',');
         val[0] = parseInt(val[0]);
         if (isNaN(val[0]) || val[0]<=0) return;  // no or invalid cookie
+
+        // The cookie is formatted as DEFAULT_INTERVAL , SERVER_TIME_WHEN_IT_WAS_ISSUED
+        // We save the server time in local storage. If the stored time differs from
+        // the cookie time we are in a new session. Hence, we have to reset all stored
+        // data and to ask the user to check the reality-check frequency.
 
         if (val[1] && val[1] != persistentStore.get('reality_check.srvtime')) {
             persistentStore.set('reality_check.srvtime', val[1]);
@@ -12830,7 +12836,7 @@ WSTickDisplay.updateChart = function(data){
         }, alrm);
     };
 
-    RealityCheck.prototype.fire = function () {
+    RealityCheck.prototype._fire = function () {
         var that = this;
 
         $.ajax({
@@ -12846,6 +12852,10 @@ WSTickDisplay.updateChart = function(data){
                 }, 5000);
             },
         });
+    };
+
+    RealityCheck.prototype.fire = function () {
+        this._fire(reality_check_url, this.display);
     };
 
     RealityCheck.prototype.display = function (data) {
@@ -12884,6 +12894,46 @@ WSTickDisplay.updateChart = function(data){
 
         $('#reality-check .blogout').on('click', function () {
             window.location.href = that.logoutLocation;
+        });
+    };
+
+    // On session start we need to ask for the reality-check interval.
+    // This is an ajax call because it depends on the user's language.
+
+    RealityCheck.prototype.askForFrequency = function () {
+        this._fire(reality_freq_url, this.displayFrequencyChoice);
+    };
+
+    RealityCheck.prototype.displayFrequencyChoice = function (data) {
+        var that = this, outer, middle, storage_handler; 
+
+        outer = $('#reality-check');
+        if (outer) outer.remove();
+
+        outer = $("<div id='reality-check' class='lightbox'></div>").appendTo('body');
+        middle = $('<div />').appendTo(outer);
+        $('<div>' + data + '</div>').appendTo(middle);
+
+        storage_handler = function (jq_event) {
+            var ack;
+
+            if (jq_event.originalEvent.key !== 'reality_check.ack') return;
+            ack = parseInt(jq_event.originalEvent.newValue || 1);
+            if (ack > that.lastAck) {
+                $(window).off('storage', storage_handler);
+                $('#reality-check').remove();
+            }
+        };
+
+        // in case the client works with multiple windows, check if he has acknowleged
+        // it in another window.
+        $(window).on('storage', storage_handler);
+
+        this.lastAck = parseInt(this.storage.get('reality_check.ack') || 1);
+        $('#reality-check [bcont=1]').on('click', function () {
+            that.storage.set('reality_check.ack', that.lastAck+1);
+            $(window).off('storage', storage_handler);
+            $('#reality-check').remove();
         });
     };
 
